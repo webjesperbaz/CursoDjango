3d5e30e4672a3742ce0466cd1d2414de0d674c31
jesperbaz <74138336+githubrepositorymio@users.noreply.github.com>
2026-01-08 19:20:23 +0100

    5º, simplicamos el renderizado y la carga de la plantilla. con la importacion de django.shortents, solo necesitariamos el return(request, 'nombre de la plantilla y diccionario del contexto'

--- Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from datetime import datetime
 from django.template import Template, Context, loader
+from django.shortcuts import render     #la manera mas facil para renderizar
 
 def saludo(request):        #primera vista
     return HttpResponse("hola wey")
@@ -16,13 +17,12 @@ def despedida(request):        #segunda vista
     # doc_externo= open("C:/Users/perea/Desktop/django/cursoDjango/Proyecto1/Proyecto1/plantillas/miplantilla.html")  #cargo documento externo
     # plt=doc_externo.read()     #lo abro y lo lee
     # doc_externo.close()         #lo cierra  
-    doc_externo= loader.get_template('miplantilla.html') #esto nos ahorra todo lo de arriba. Pero hay que cargar la clase y en el archivo setting.py poner la ruta de la carpeta plantillas en el apartado 'DIRS'
-
+    # doc_externo= loader.get_template('miplantilla.html') #esto nos ahorra todo lo de arriba. Pero hay que cargar la clase y en el archivo setting.py poner la ruta de la carpeta plantillas en el apartado 'DIRS'
     # plt = Template(plt)         # Convertimos el texto plano en un objeto Template de Django 
-    
     # ctx = Context({"nombre_persona": nombre, "apellido_persona": apellido, "dia_actual": dia, "temas": temasDelCurso})       #creo el contexto, siempre en diccionario obligatorio aunque esté vacio
-    documento = doc_externo.render({"nombre_persona": nombre, "apellido_persona": apellido, "dia_actual": dia, "temas": temasDelCurso})                 #renderizamos el documemto y en esta nueva manera de hacerlo le pasamos el diccionario de contexto aqui diretamente
-    return HttpResponse(documento)
+    # documento = doc_externo.render({"nombre_persona": nombre, "apellido_persona": apellido, "dia_actual": dia, "temas": temasDelCurso})                 #renderizamos el documemto y en esta nueva manera de hacerlo le pasamos el diccionario de contexto aqui diretamente
+    
+    return render(request, "miplantilla.html",{"nombre_persona": nombre, "apellido_persona": apellido, "dia_actual": dia, "temas": temasDelCurso} )
 
 def dameFecha (request):            #tercera vista, esta será dinamica ya que la hora ira cambiando
     fecha_actual= datetime.now()
