ddc424092d0ca8766e775721366caec75d80f56e
jesperbaz <74138336+githubrepositorymio@users.noreply.github.com>
2026-01-18 22:04:14 +0100

    12º, formulario que recoge informacion, se envia al servidor y este contesta

--- TiendaOnline/gestionPedidos/views.py
+++ TiendaOnline/gestionPedidos/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here. Vistas:
+
+def busqueda_productos(request):
+    return render(request, "busqueda_productos.html" )
+
+def buscar(request):
+    if request.GET["prd"]:
+        
+        mensaje= "Articulo buscado es : %r" %request.GET["prd"]         #informacion que le viene desde el formulario
+    
+    else:
+        mensaje="No has introducido nada."
+        
+    return HttpResponse(mensaje)
