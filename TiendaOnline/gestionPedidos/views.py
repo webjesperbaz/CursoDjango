b9b1d160d90215296e8adea1b52430d77a288828
jesperbaz <74138336+githubrepositorymio@users.noreply.github.com>
2026-01-19 12:05:12 +0100

    13º, busqueda en la base de datos y mostrar la informacion solicitada desde el formulario

--- TiendaOnline/gestionPedidos/views.py
+++ TiendaOnline/gestionPedidos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from gestionPedidos.models import Articulos
 
 # Create your views here. Vistas:
 
@@ -9,8 +10,14 @@ def busqueda_productos(request):
 def buscar(request):
     if request.GET["prd"]:
         
-        mensaje= "Articulo buscado es : %r" %request.GET["prd"]         #informacion que le viene desde el formulario
-    
+        # mensaje= "Articulo buscado es : %r" %request.GET["prd"]         #informacion que le viene desde el formulario
+        producto= request.GET["prd"]
+
+        articulos= Articulos.objects.filter(nombre__icontains= producto)    #busca consulta con sql, lo que recoja del formulario, y (nombre__icontrains= producto) buscará todo lo que en Articulos contenga en nombre esa palabra.
+
+        return render(request, "resultados_busqueda.html", {"articulos":articulos, "query":producto})
+
+
     else:
         mensaje="No has introducido nada."
         
